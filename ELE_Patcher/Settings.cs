8f7033fa8e8704763695b2d569318281f6c3c9bb
kuplion <kuplion@hotmail.com>
2024-12-26 00:59:48 +0000

    Updated settings text to reference Lux

--- ELE_Patcher/Settings.cs
+++ ELE_Patcher/Settings.cs
@@ -4,7 +4,7 @@ namespace ELE_Patcher
 {
 	public record Settings
 	{
-		[Tooltip("Most ELE conflicts can be solved by putting ELE late in the load order. Select this to skip patching records from ELE_SSE.esp, can save some time.")]
+		[Tooltip("Most Lux conflicts can be solved by putting Lux late in the load order. Select this to skip patching records from Lux.esp, can save some time.")]
 		public bool SkipVanilla = false;
 	}
 }
